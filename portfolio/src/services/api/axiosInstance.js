a40bc4b1d16976babe00ad0636bde68036499d45
nawafali01 <nawafali3435@gmail.com>
2026-07-16 13:46:47 +0500

    fix production erorr

--- portfolio/src/services/api/axiosInstance.js
+++ portfolio/src/services/api/axiosInstance.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const baseURL = import.meta.env.VITE_API_URL;
+const baseURL = import.meta.env.VITE_API_URL || (import.meta.env.PROD 
+  ? (function() { throw new Error("VITE_API_URL environment variable is missing for production build!"); })()
+  : "http://localhost:5000/api");
 
 export const axiosInstance = axios.create({
   baseURL,
@@ -8,5 +10,5 @@ export const axiosInstance = axios.create({
     "Content-Type": "application/json",
     "ngrok-skip-browser-warning": "true",
   },
-  timeout: 10000,
+  timeout: 30000,
 });
